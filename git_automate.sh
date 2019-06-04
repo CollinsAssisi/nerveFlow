bed69b1b3765f314b8a48da3d69da0e0f348c00a
Collins Assisi <collins@iiserpune.ac.in>
2019-06-04 09:24:16 +0530

    Jun_04_19_09:24:16AM

--- git_automate.sh
+++ git_automate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd /data/projects/nerveFlowv2
-
 find ./* -size +100M | cat >> .gitignore
 
-sudo git add .
+git add .
 
-sudo git commit -m $(printf "%s" $(date +"%h_%d_%y_%r"))
+git commit -m $(printf "%s" $(date +"%h_%d_%y_%r"))
 
-sudo git push origin master
+git push origin master
